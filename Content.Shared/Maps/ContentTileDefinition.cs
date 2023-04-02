ac7dfb9c130686ee3131fbe1f7838dbf5d381341
Pspritechologist <81725545+Pspritechologist@users.noreply.github.com>
2023-04-02 12:24:36 -0400

    Sound collections can now be specified per tile, to allow for dynamic switching of ambient sound. (#7)
    Sounds are temporary. (i.e. They suck)

--- Content.Shared/Maps/ContentTileDefinition.cs
+++ Content.Shared/Maps/ContentTileDefinition.cs
@@ -75,6 +75,8 @@ namespace Content.Shared.Maps
         /// </summary>
         [DataField("weather")] public bool Weather = false;
 
+        [DataField("tileAmbientSound")] public String? TileAmbientSound { get;}
+
         public void AssignTileId(ushort id)
         {
             TileId = id;
